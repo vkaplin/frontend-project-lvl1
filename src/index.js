f594d84af0cf676e3a46e3b04463b8e7746d3a81
Kaplin Vladimir <hawk.hawk30@gmail.com>
2022-04-29 00:19:54 +0300

    fix lint error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { getUserName, checkAnswer, getUserAnswer } from './brain-core.js';
-//import { games } from './games-type.js';
+
 import getCorrectAnswerCalc from './games/calc.js';
 import getCorrectAnswerEven from './games/even.js';
 import getCorrectAnswerGCD from './games/gcd.js';
@@ -14,8 +14,6 @@ const games = {
   prime: 'prime',
 };
 
-
-
 function startGame(gameType) {
   let curretnAttempt = 0;
   let answerIsCorrect = false;
@@ -25,41 +23,41 @@ function startGame(gameType) {
   console.log(`Hello, ${userName}`);
 
   do {
-  let correctAnswer;
+    let correctAnswer;
 
-  switch (gameType) {
-    case games.calc:
-    correctAnswer = getCorrectAnswerCalc();
-    break;
-    case games.even:
-    correctAnswer = getCorrectAnswerEven();
-    break;
-    case games.gcd:
-    correctAnswer = getCorrectAnswerGCD();
-    break;
-    case games.progression:
-    correctAnswer = getCorrectAnswerProgression();
-    break;
-    case games.prime:
-    correctAnswer = getCorrectAnswerPrime();
-    break;
-    default:
-    console.log('type not found');
-  }
+    switch (gameType) {
+      case games.calc:
+        correctAnswer = getCorrectAnswerCalc();
+        break;
+      case games.even:
+        correctAnswer = getCorrectAnswerEven();
+        break;
+      case games.gcd:
+        correctAnswer = getCorrectAnswerGCD();
+        break;
+      case games.progression:
+        correctAnswer = getCorrectAnswerProgression();
+        break;
+      case games.prime:
+        correctAnswer = getCorrectAnswerPrime();
+        break;
+      default:
+        console.log('type not found');
+    }
 
-  const userAnswer = getUserAnswer();
-  answerIsCorrect = checkAnswer(correctAnswer, userAnswer);
+    const userAnswer = getUserAnswer();
+    answerIsCorrect = checkAnswer(correctAnswer, userAnswer);
 
-  if (curretnAttempt === 2 && answerIsCorrect) {
-    console.log(`Congratulations, ${userName}!`);
-  } else if (answerIsCorrect) {
-    console.log('Correct!');
-  } else {
-    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-    console.log(`Let's try again, ${userName}!`);
-  }
+    if (curretnAttempt === 2 && answerIsCorrect) {
+      console.log(`Congratulations, ${userName}!`);
+    } else if (answerIsCorrect) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+    }
 
-  curretnAttempt += 1;
+    curretnAttempt += 1;
   } while (curretnAttempt < 3 && answerIsCorrect);
 }
 
