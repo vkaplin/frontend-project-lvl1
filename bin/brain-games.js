13675d9c3d5ca7ccfc43af0e87741449e74c8229
Kaplin Vladimir <hawk.hawk30@gmail.com>
2022-05-04 14:38:19 +0300

    изменил перенес логику в файл cli.js в файле brain-games.js только вызов

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
-import getUserName from '../src/cli.js';
+import sayHelloToUser from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = getUserName();
-
-console.log(`Hello, ${userName}`);
+sayHelloToUser();
